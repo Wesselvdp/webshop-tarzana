238aef27f0daac93e0410700c919ef6afaf05200
Wessel <wesselvdp@gmail.com>
2020-05-23 12:53:44 +0200

    fixing netlify build error

--- components/ui/EmailSubmit.tsx
+++ components/ui/EmailSubmit.tsx
@@ -1,21 +1,20 @@
-import React, { FC } from 'react'
-import {Formik} from 'Formik'
+import React, { FC } from "react";
+import { Formik } from "formik";
 
-type T = any
+type T = any;
 
 const EmailSubmit: FC<T> = () => {
   return (
-
- <Formik
-      initialValues={{ email: ''}}
-      validate={values => {
+    <Formik
+      initialValues={{ email: "" }}
+      validate={(values) => {
         const errors = {};
         if (!values.email) {
-          errors.email = 'Required';
+          errors.email = "Required";
         } else if (
           !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
         ) {
-          errors.email = 'Invalid email address';
+          errors.email = "Invalid email address";
         }
         return errors;
       }}
@@ -51,8 +50,7 @@ const EmailSubmit: FC<T> = () => {
         </form>
       )}
     </Formik>
+  );
+};
 
-  )
-}
-
-export default EmailSubmit
+export default EmailSubmit;
